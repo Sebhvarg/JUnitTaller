b44c1a091b453e732b13132e83a3f039b0e592fb
CrisPih <152130690+CrisPih@users.noreply.github.com>
2024-08-15 23:36:51 -0500

    Correcciones EmployeeTest.java

--- SeccionB/src/tallerpruebas/EmployeeTest.java
+++ SeccionB/src/tallerpruebas/EmployeeTest.java
@@ -1,11 +1,13 @@
 package tallerpruebas;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +18,7 @@ class EmployeeTest {
     private Employee supervisorNonUSD;
     private Employee managerUSD;
     private Employee managerNonUSD;
+
     
     
     @BeforeEach
@@ -28,119 +31,122 @@ class EmployeeTest {
         managerNonUSD = new Employee(3000, "NSD", 20, EmployeeType.Manager);
 
     }
-	    @Test
-	    public void testWorkerUSDMesPar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 == 0) {
-	            float expectedSalary = 1000;
-	            assertEquals(expectedSalary, WorkerUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    @Test
-	    public void testWorkerUSDMesImpar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 != 0) {
-	            float expectedSalary = 1000 + (386.0f / 12 * 2);
-	            assertEquals(expectedSalary, WorkerUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    
-	    @Test
-	    public void testEmployeeNonUSDMesPar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 == 0) {
-	            float expectedSalary = 1000 * 0.95f;
-	            assertEquals(expectedSalary, WorkerNonUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    @Test
-	    public void testEmployeeNonUSDMesImpar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 == 0) {
-	            float expectedSalary = 1000 * 0.95f;
-	            assertEquals(expectedSalary, WorkerNonUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    //////
-	    @Test
-	    public void testSupervisorUSDMesPar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 == 0) {
-	            float expectedSalary = 1000;
-	            assertEquals(expectedSalary, supervisorUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    @Test
-	    public void testSupervisorUSDMesImpar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 != 0) {
-	            float expectedSalary = 1000 + (386.0f / 12 * 2);
-	            assertEquals(expectedSalary, supervisorUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    
-	    @Test
-	    public void testSupervisorNonUSDMesPar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 == 0) {
-	            float expectedSalary = 1000 * 0.95f;
-	            assertEquals(expectedSalary, supervisorNonUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    @Test
-	    public void testSupervisorNonUSDMesImpar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 == 0) {
-	            float expectedSalary = 1000 * 0.95f;
-	            assertEquals(expectedSalary, supervisorNonUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    
-	    
-	    @Test
-	    public void testManagerUSDMesPar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 == 0) {
-	            float expectedSalary = 1000;
-	            assertEquals(expectedSalary, managerUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    @Test
-	    public void testManagerUSDMesImpar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 != 0) {
-	            float expectedSalary = 1000 + (386.0f / 12 * 2);
-	            assertEquals(expectedSalary, managerUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    
-	    @Test
-	    public void testManagerNonUSDMesPar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 == 0) {
-	            float expectedSalary = 1000 * 0.95f;
-	            assertEquals(expectedSalary, managerNonUSD.cs(), 0.01);
-	        }
-	    }
-	    
-	    @Test
-	    public void testManagerNonUSDMesImpar() {
-	        LocalDate currentDate = LocalDate.now();
-	        if (currentDate.getMonthValue() % 2 == 0) {
-	            float expectedSalary = 1000 * 0.95f;
-	            assertEquals(expectedSalary, managerNonUSD.cs(), 0.01);
-	        }
-	    }
+    
+    // empleado tipo Worker
+    
+    @Test
+    public void testWorkerUSDMesPar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 == 0) {
+            float expectedSalary = 1000;
+            assertEquals(expectedSalary, WorkerUSD.cs(), 0.01);
+        }
+    }    
+    
+    
+    @Test
+    public void testWorkerUSDMesImpar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 != 0) {
+            float expectedSalary = 1000 + (386.0f / 12 * 2);
+            assertEquals(expectedSalary, WorkerUSD.cs(), 0.01);
+        }
+    }
+    
+    @Test
+    public void testWorkerNonUSDMesPar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 == 0) {
+            float expectedSalary = 1000 * 0.95f;
+            assertEquals(expectedSalary, WorkerNonUSD.cs(), 0.01);
+        }
+    }
+    
+    @Test
+    public void testWorkerNonUSDMesImpar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 != 0) {
+            float expectedSalary = 1000 * 0.95f + (386.0f / 12 * 2);
+            assertEquals(expectedSalary, WorkerNonUSD.cs(), 0.01);
+        }
+    }
+    
+    
+    // empleado tipo supervisor
+    
+    @Test
+    public void testSupervisorUSDMesPar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 == 0) {
+            float expectedSalary = 2000 + (2000 * 0.35f);
+            assertEquals(expectedSalary, supervisorUSD.cs(), 0.01);
+        }
+    }
+
+    @Test
+    public void testSupervisorUSDMesImpar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 != 0) {
+            float expectedSalary = 2000 + (2000 * 0.35f) + (386.0f / 12 * 2);
+            assertEquals(expectedSalary, supervisorUSD.cs(), 0.01);
+        }
+    }
+
+    @Test
+    public void testSupervisorNonUSDMesPar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 == 0) {
+            float expectedSalary = 2000 * 0.95f + (2000 * 0.95f * 0.35f);
+            assertEquals(expectedSalary, supervisorNonUSD.cs(), 0.01);
+        }
+    }
+
+    @Test
+    public void testSupervisorNonUSDMesImpar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 != 0) {
+            float expectedSalary = 2000 * 0.95f + (2000 * 0.95f * 0.35f) + (386.0f / 12 * 2);
+            assertEquals(expectedSalary, supervisorNonUSD.cs(), 0.01);
+        }
+    }
+
+    // empleado tipo Manager
+    
+    @Test
+    public void testManagerUSDMesPar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 == 0) {
+            float expectedSalary = 3000 + (3000 * 0.7f);
+            assertEquals(expectedSalary, managerUSD.cs(), 0.01);
+        }
+    }
+
+    @Test
+    public void testManagerUSDMesImpar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 != 0) {
+            float expectedSalary = 3000 + (3000 * 0.7f) + (386.0f / 12 * 2);
+            assertEquals(expectedSalary, managerUSD.cs(), 0.01);
+        }
+    }
+
+    @Test
+    public void testManagerNonUSDMesPar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 == 0) {
+            float expectedSalary = 3000 * 0.95f + (3000 * 0.95f * 0.7f);
+            assertEquals(expectedSalary, managerNonUSD.cs(), 0.01);
+        }
+    }
+
+    @Test
+    public void testManagerNonUSDMesImpar() {
+        LocalDate currentDate = LocalDate.now();
+        if (currentDate.getMonthValue() % 2 != 0) {
+            float expectedSalary = 3000 * 0.95f + (3000 * 0.95f * 0.7f) + (386.0f / 12 * 2);
+            assertEquals(expectedSalary, managerNonUSD.cs(), 0.01);
+        }
+    }
 	    
 	    @Test
 		void employeeUSDTestCalculateYearBonus() {
